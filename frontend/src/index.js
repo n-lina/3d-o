e8caf65eac1619e55d0a90400b959bb6b5312c74
unknown <linanguyen@hotmail.com>
2020-12-07 22:54:24 -0800

    skeleton

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { onPatch } from "mobx-state-tree";
+import makeInspectable from "mobx-devtools-mst";
+import Invoice from "./models/Invoice";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const invoice = Invoice.create({ currency: "CAD" });
+
+onPatch(invoice, patch => {
+  console.log(patch);
+});
+makeInspectable(invoice);
+
+ReactDOM.render(<App invoice={invoice} />, document.getElementById("root"));
+// registerServiceWorker();
