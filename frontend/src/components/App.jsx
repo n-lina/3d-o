e3ab5d051bf2abb92f5e9a12319b3fa61bb701b9
unknown <linanguyen@hotmail.com>
2020-12-10 23:50:32 -0800

    styling and navbar added

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,63 +1,68 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
 import InvoiceItem from "./InvoiceItem";
+import Navbar from "./Navbar/Navbar"
+import '../App.css'
 
 class App extends Component {
   render() {
     const { invoice } = this.props;
 
     return (
-      <div className="App">
-        <h1>Status: {invoice.status()}</h1>
+      <div className="Background">
+        <div className="InnerApp">
+          <Navbar />
+          <h1>Status: {invoice.status()}</h1>
 
-        {!invoice.is_paid && (
-          <button
-            onClick={e => {
+          {!invoice.is_paid && (
+            <button
+              onClick={e => {
+                e.preventDefault();
+                invoice.markPaid();
+              }}
+            >
+              Pay
+            </button>
+          )}
+
+          <form
+            onSubmit={e => {
               e.preventDefault();
-              invoice.markPaid();
+              invoice.itemList.add({
+                quantity: this.quantityInput.value,
+                name: this.nameInput.value,
+                price: this.priceInput.value
+              });
+              e.target.reset();
+              this.nameInput.focus();
             }}
           >
-            Pay
-          </button>
-        )}
+            <label>
+              Name
+              <input ref={input => (this.nameInput = input)} />
+            </label>
 
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            invoice.itemList.add({
-              quantity: this.quantityInput.value,
-              name: this.nameInput.value,
-              price: this.priceInput.value
-            });
-            e.target.reset();
-            this.nameInput.focus();
-          }}
-        >
-          <label>
-            Name
-            <input ref={input => (this.nameInput = input)} />
-          </label>
+            <label>
+              Quantity
+              <input ref={input => (this.quantityInput = input)} />
+            </label>
 
-          <label>
-            Quantity
-            <input ref={input => (this.quantityInput = input)} />
-          </label>
+            <label>
+              Price
+              <input ref={input => (this.priceInput = input)} />
+            </label>
 
-          <label>
-            Price
-            <input ref={input => (this.priceInput = input)} />
-          </label>
+            <button type="submit">Add</button>
+          </form>
 
-          <button type="submit">Add</button>
-        </form>
+          <strong>Total Cost ${invoice.itemList.total().toFixed(2)}</strong>
 
-        <strong>Total Cost ${invoice.itemList.total().toFixed(2)}</strong>
-
-        <ul>
-          {invoice.itemList.items.map((item, i) => (
-            <InvoiceItem key={i} item={item} />
-          ))}
-        </ul>
+          <ul>
+            {invoice.itemList.items.map((item, i) => (
+              <InvoiceItem key={i} item={item} />
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
