13b767aabe509dc5c3cd336ba513ccc02fef0503
unknown <linanguyen@hotmail.com>
2020-12-26 13:50:46 -0800

    set up threejs and react three fiber

--- frontend/src/pages/create-vase.js
+++ frontend/src/pages/create-vase.js
@@ -3,6 +3,8 @@ import {
   NavBtn,
   NavBtnLink
 } from '../components/Navbar/NavbarElements';
+import { Canvas} from "react-three-fiber";
+import Box from "../components/Box"
 
 const CreateVase = () => {
   return (
@@ -18,11 +20,18 @@ const CreateVase = () => {
       >
         <h1>Vase</h1>
       </div>
-      <div style={{display: 'flex', flexDirection:'row', width: 'auto', height: '90vh'}}>
+      <div style={{display: 'flex', flexDirection:'row', width: 'auto', height: '100vh'}}>
         <div style={{background: '#baa', width: '57%', height: 'auto'}}>
-          <h1>Canvas</h1>
+          {/* <h1>Canvas</h1> */}
+          <Canvas style={{background:"#fee"}}>
+            <ambientLight intensity={0} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[2.5, 0, 0]} />
+          </Canvas>
         </div>
-        <div style={{background: '#daa', width: '43%', height: 'auto'}}>
+        <div style={{background: '#daa', width: '44%', height: 'auto'}}>
           <h1>Input</h1>
           <NavBtn>
             <NavBtnLink to='/colouring'>Done</NavBtnLink>
