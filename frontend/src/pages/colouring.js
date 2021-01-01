af42de95f0b31b252f4b9fec5aa4b216f8fe02a3
unknown <linanguyen@hotmail.com>
2021-01-01 01:38:39 -0800

    coloring page setup

--- frontend/src/pages/colouring.js
+++ frontend/src/pages/colouring.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { CirclePicker } from "react-color";
+import DrawingSection from "../components/DrawingSection";
+import "./colouringPage.css";
+import LineTo from 'react-lineto';
 
 const Colouring = () => {
+  const [panelWidth, setPanelWidth] = useState(16);
+  const [panelHeight, setPanelHeight] = useState(16);
+  const [selectedColor, setColor] = useState("#f44336");
+
+  function changeColor(color) {
+    setColor(color.hex);
+  }
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 'auto',
-        width: 'auto'
-      }}
-    >
-      <h1>Colouring</h1>
-    </div>
+    <div>
+      <div className = "toolbar" style={{background: '#fae', justifyContent:'center', alignItems: 'center', display:'flex'}}>
+        <CirclePicker color={selectedColor} onChangeComplete={changeColor} />
+      </div>
+      <div className = "canvas"
+        style={{
+          // display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: 'auto',
+          width: 'auto', 
+          // background: '#baf'
+        }}
+      >
+        <DrawingSection
+          width={panelWidth}
+          height={panelHeight}
+          selectedColor={selectedColor}
+        />
+        <DrawingSection
+          width={panelWidth}
+          height={panelHeight}
+          selectedColor={selectedColor}
+        />
+        <h1 className="hello">hello</h1>
+        <br/>
+        <h1 className="bye">hello</h1>
+        <LineTo borderWidth={100} from="hello" to="bye" borderStyle="solid" delay={500}/>
+        <LineTo borderWidth={100} from="hello" to="toolbar" borderStyle="solid"/>
+      </div>
+  </div>
   );
 };
 
