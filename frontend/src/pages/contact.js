f61b3714291ba2ab05d0f632906962325af4c250
unknown <linanguyen@hotmail.com>
2021-01-12 00:12:40 -0800

    contact pg

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import bg from "../assets/logo-bg-cropped.png"
 import bgOff from "../assets/logo-bg-off.png"
+import straw from "../assets/strawberry-slider-big.png"
+import "./create-vase.css"
 
 const Contact = () => {
   return (
     <div
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '80vh',
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        height: '464px',
         backgroundImage: `url(${bg})`, 
         width: "98%", 
         marginLeft: "1%"
       }}
     >
-      <div style={{background: "white", height: 200, width: 400, borderRadius: 40, borderColor: "red", borderStyle: "solid"}}></div>
+      <div className="info-box" >
+      <img src={straw} className ="straw-l"/>
+        <img src={straw} className ="straw-r"/>
+      </div>
     </div>
   );
 };
