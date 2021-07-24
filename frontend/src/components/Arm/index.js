9da098610f980644c53eba8cb42b6db3903c97c3
unknown <linanguyen@hotmail.com>
2021-07-23 23:48:49 -0700

    added arm dimensions

--- frontend/src/components/Arm/index.js
+++ frontend/src/components/Arm/index.js
@@ -4,7 +4,14 @@ import "../DrawingSection/drawingSection.css"
 
 const Arm = (props) => {
     const {caption, size, formObject} = props;
-    const dimensions = [10,4] // top to bottom 
+    const arms = {
+        small: [10,4],
+        med: [15,6], 
+        big: [20,8]
+    }
+    let dimensions = arms.med
+    if (size == "small") dimensions = arms.small
+    else if (size == "big") dimensions = arms.large
     const object = <UntrackedDrawingSection arm={true} specialTop = {[]} specialBottom = {[]} formObject={formObject} dimensions={dimensions}/>
 
     return (
